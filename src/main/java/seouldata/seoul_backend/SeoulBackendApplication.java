e610ab46fa2da0623f7e3499ecc9661a9a59d483
lovee9523 <noeun1102@gmail,com>
2024-04-01 01:14:58 +0900

    Initial commit

---
+++ src/main/java/seouldata/seoul_backend/SeoulBackendApplication.java
@@ -0,0 +1,13 @@
+package seouldata.seoul_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seouldata/seoul_backend/SeoulBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package seouldata.seoul_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
