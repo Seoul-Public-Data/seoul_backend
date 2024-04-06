23c77fc6b69d6ee58c27445ccb77f31a184ab886
lovee9523 <noeun1102@gmail,com>
2024-04-06 22:31:31 +0900

    [feat] an_cctv 테이블과 엔티티 맵핑

---
+++ src/main/java/seouldata/seoul_backend/cctv/domain/entity/AnCctv.java
@@ -0,0 +1,34 @@
+package seouldata.seoul_backend.cctv.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "an_cctv")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AnCctv {
+
+    @Id
+    private Long number;
+    private String autonomousRegion;
+    private String address;
+    private Double lon;
+    private Double lat;
+
+    @Builder
+    public AnCctv(Long number, String autonomousRegion, String address,
+                  Double lon, Double lat) {
+        this.number = number;
+        this.autonomousRegion = autonomousRegion;
+        this.address = address;
+        this.lon = lon;
+        this.lat = lat;
+    }
+
+}
